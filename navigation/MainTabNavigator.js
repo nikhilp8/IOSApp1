25b2335ff3823411276667a35043f66a13ddec49
Nikhil Ponugoti <nikhilponugoti@nikhils-mbp.attlocal.net>
2019-04-23 19:51:09 -0500

    basic workout screen

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -4,15 +4,34 @@ import { createStackNavigator, createBottomTabNavigator } from 'react-navigation
 
 import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
+import WorkoutsScreen from '../screens/WorkoutsScreen';
 import LinksScreen from '../screens/LinksScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 
+// const HomeStack = createStackNavigator({
+//   Home: HomeScreen,
+// });
+
+// HomeStack.navigationOptions = {
+//   tabBarLabel: 'Home',
+//   tabBarIcon: ({ focused }) => (
+//     <TabBarIcon
+//       focused={focused}
+//       name={
+//         Platform.OS === 'ios'
+//           ? `ios-information-circle${focused ? '' : '-outline'}`
+//           : 'md-information-circle'
+//       }
+//     />
+//   ),
+// };
+
 const HomeStack = createStackNavigator({
-  Home: HomeScreen,
+  Home: WorkoutsScreen,
 });
 
 HomeStack.navigationOptions = {
-  tabBarLabel: 'Home',
+  tabBarLabel: 'Workouts',
   tabBarIcon: ({ focused }) => (
     <TabBarIcon
       focused={focused}
